db62f0a7f67fd177e48a7eef9ca818a926ce5bfd
Alan Mauricio Reyes Telesforo <idgsmauricioreyes@gmail.com>
2026-07-30 12:32:41 -0600

    social

--- src/data/social.ts
+++ src/data/social.ts
@@ -3,5 +3,5 @@ import linkedinIcon from "../assets/social/icons8-linkedin-96.svg";
 
 export const socialLinks = [
     { name: "GitHub", url: "https://github.com/MauricioDev1998", icon: githubIcon, dark: true },
-    { name: "LinkedIn", url: "https://www.linkedin.com/in/mauricio-reyes", icon: linkedinIcon },
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/mauricio-reyes-idgs", icon: linkedinIcon },
 ];
